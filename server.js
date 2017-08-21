85e3d47e5b7303a91a68fe060df3a75deee7dfe0
25prithvi <prithvi25@hotmail.com>
2017-08-21 23:53:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var morgan = require('morgan');
+var express = require('express'); //create webserver and handle ports and https
+var morgan = require('morgan'); //
 var path = require('path');
 
 var app = express();
