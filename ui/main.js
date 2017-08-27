a4ce0958e9031b96f1a25664d94f6f1570547751
25prithvi <prithvi25@hotmail.com>
2017-08-27 23:44:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,9 +12,9 @@ var img = document.getElementById('madi');
 var marginLeft = 0;
 
 function moveRight(){
-    marginLeft = marginLeft + 1;
+    marginLeft = marginLeft + 100;
     img.style.marginLeft = marginLeft +'px';
 }
 img.onclick = function (){
-    var interval = setInterval(moveRight,10);
+    var interval = setInterval(moveRight,1000);
 };
